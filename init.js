3a014b12a519657519e1b706250a475794c2eb28
Rafael Gieschke <rafael@gieschke.de>
2021-10-27 18:06:36 +0200

    Update

--- init.js
+++ init.js
@@ -25,7 +25,10 @@ class Machine {
     console.debug(component, spec);
   }
   start() {
-
+    `DISPLAY=:0 ./SheepShaver --nosound true --rom /tmp/.X11-unix/xxx.rom  --disk /tmp/.X11-unix/8xxx.raw --ether vde --switch /tmp/.X11-unix/ --ramsize 268435456 --jit true --ignoresegv true --ignoreillegal true --jit68k true --idlewait true`;
+    `vde_switch -s /tmp/.X11-unix/`;
+    `slirpvde -s /tmp/.X11-unix/`;
+    `vde_plug -s /tmp/.X11-unix/`;
   }
 }
 
